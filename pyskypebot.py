9b22a0bed6dc1993ff6636f62d63030e889865cc
Sriram Keerthi M K <sriram@raremile.com>
2015-06-25 12:19:19 +0530

    Added sendMessage functionality

--- pyskypebot.py
+++ pyskypebot.py
@@ -1,3 +1,4 @@
+import argparse
 import time
 import sys, getopt
 from selenium import webdriver
@@ -5,14 +6,16 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.keys import Keys
 
 driver = None
 
 def launch():
   print("Loading...")
   global driver
-  # driver = webdriver.PhantomJS("phantomjs-2.0.0-windows\\bin\\phantomjs.exe")
-  driver = webdriver.Chrome()
+  driver = webdriver.PhantomJS("phantomjs-2.0.0-windows\\bin\\phantomjs.exe")
+  driver.set_window_size(1024, 768)
+  # driver = webdriver.Chrome()
   driver.get('https://web.skype.com/')
   print("Loaded Skype!")
 
@@ -40,13 +43,42 @@ def quit():
   print("Quitting...")
   driver.quit()
 
+def recents():
+  return driver.find_elements_by_tag_name("swx-recent-item")
 
-def main(argv):
+def main(args):
   try:
-    userName = ""
-    password = ""
     launch()
-    signIn(userName, password)
+    signIn(args.username, args.password)
     signOut()
+  except:
+    print("Something went wrong")
   finally:
     quit()
+
+def sendMessageToSelected(message):
+  element = WebDriverWait(driver, 30).until(EC.element_to_be_clickable((By.TAG_NAME,'swx-textarea')))
+  element = element.find_element_by_tag_name("textarea")
+  element.click()
+  element.clear()
+  element.send_keys(message)
+  element.send_keys(Keys.ENTER)
+
+def sendMessage(tileName, message):
+  recent(tileName).click()
+  sendMessageToSelected(message)
+
+def getTileName(element):
+  return element.find_element_by_class_name("tileName").text
+
+def recent(tileName):
+  for recentTile in recents():
+    if getTileName(recentTile) == tileName:
+      return recentTile
+
+if __name__ == "__main__":
+  print("Starting up...")
+  parser = argparse.ArgumentParser(description='pySkypeBot: Python Skype Bot')
+  parser.add_argument('-u', '--username', help='Skype User Name')
+  parser.add_argument('-p', '--password', help='Skype Password')
+  main(parser.parse_args())
